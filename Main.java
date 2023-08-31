1fd5d69a467a8009cd81a7c4e36a61dbe6848ea2
Jade Lawson <jade.lawson@live.com>
2023-08-30 20:46:43 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    /*
+     * Student name: Jade Lawson
+     * Course and section: INEW-2338-21700
+     * Assignment: 0
+     * My program code satisfies the requirements of the assignment by
+     * successfully running an output of 'Hello World' as noted in the assignment.
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
